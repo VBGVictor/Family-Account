816fcea7519ec7dd3f1e349509317a58ab9b9aab
Victor Barbosa Goveia <victor.barbosa.gov@gmail.com>
2022-08-09 15:57:43 -0300

    First commit - Account

---
+++ src/main/java/com/FAccount/model/Account.java
@@ -0,0 +1,70 @@
+package com.FAccount.model;
+
+import java.util.Objects;
+
+public class Account {
+
+	private String bank;
+	private Integer numberAccount;
+	private Integer digitAccount;
+	private Integer agency;
+	private Integer numberBank;
+	
+	public Account() {
+	}
+
+	public Account(String bank, Integer numberAccount, Integer digitAccount, Integer agency, Integer numberBank) {
+		super();
+		this.bank = bank;
+		this.numberAccount = numberAccount;
+		this.digitAccount = digitAccount;
+		this.agency = agency;
+		this.numberBank = numberBank;
+	}
+
+	public String getBank() {
+		return bank;
+	}
+
+	public Integer getNumberAccount() {
+		return numberAccount;
+	}
+
+	public Integer getDigitAccount() {
+		return digitAccount;
+	}
+
+	public Integer getAgency() {
+		return agency;
+	}
+
+	public Integer getNumberBank() {
+		return numberBank;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(digitAccount, numberAccount);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Account other = (Account) obj;
+		return Objects.equals(digitAccount, other.digitAccount) && Objects.equals(numberAccount, other.numberAccount);
+	}
+
+	@Override
+	public String toString() {
+		return "Account [bank=" + bank + ", numberAccount=" + numberAccount + "-" + digitAccount
+				+ ", agency=" + agency + ", numberBank=" + numberBank + "]";
+	}
+	
+	
+	
+}
